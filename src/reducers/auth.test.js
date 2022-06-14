a1b83b0733f4d33380c8d808ceb9a787c504fb9d
Andrey Voronin <avoronin@softplus.ru>
2022-06-14 17:19:43 +0500

    Тестирование Исправлена грамматика в сообщении

--- src/reducers/auth.test.js
+++ src/reducers/auth.test.js
@@ -2,10 +2,10 @@ import auth from "./auth.js"
 import {logIn, logOut} from "../actions"
 
 describe("auth reducer", () => {
-    it("set logged in state to true on logIn action", () => {
+    it("sets logged in state to true on logIn action", () => {
         expect(auth({}, logIn())).toEqual({isLoggedIn: true})
     })
-    it("set logged in state to false on logOut action", () => {
+    it("sets logged in state to false on logOut action", () => {
         expect(auth({}, logOut())).toEqual({isLoggedIn: false})
     })    
 })
